1347b2009b2517bfea2c4bd2ff8425fecf7449bc
Enis Hetemi <Enisi@DESKTOP-2OLQ5TK>
2026-03-21 17:42:26 +0100

    final: dokumentimi i plote sipas kerkesave

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("MatchDay 5+1 API po punon!");
-});
-
-app.listen(PORT, () => {
-    console.log(`Serveri po punon në http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`MatchDay running on port ${PORT}`));
 
