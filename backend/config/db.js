46f82ebe60a16ed442c60fcbacea0013040c1157
Enis Hetemi <Enisi@DESKTOP-2OLQ5TK>
2026-04-03 13:29:10 +0100

    fix: render postgresql connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'matchday_db', 
-  password: '1234', 
-  port: 5432,
+  connectionString: process.env.DATABASE_URL || 'postgresql://matchday_db_e7i5_user:rUCUh0fhdaJvfSJMUAhb6syFYioVytTE@dpg-d77r3n9r0fns738942sg-a.oregon-postgres.render.com/matchday_db_e7i5',
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
